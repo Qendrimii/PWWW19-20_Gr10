d65aee0e491fa68843051c0082afa0f081349aeb
ErmirMulaku <37796995+ErmirMulaku@users.noreply.github.com>
2020-05-27 22:41:14 +0200

    Ndryshime

---
+++ php/form.php
@@ -0,0 +1,36 @@
+<?php
+ $vlera = true;
+ $servername = "localhost:3306";
+ $username = "root";
+ $password = "";
+ $db = "myDB"; 
+ $vlera = true;
+ $conn = mysqli_connect($servername,$username,$password,$db);
+ if(!$conn)
+ {echo "Nuk mund te lidhet me databazen";}
+ 
+
+if(isset($_POST['submit']))
+{ 
+ 
+    $foto = $_POST("foto");
+    $idPhoto = $_POST["fotoID"];
+    $emri = $_POST["emri"];
+    $mbiemri = $_POST["mbiemri"];
+    $email = $_POST["email"];
+  /*  $tema = $_POST["tema"];*/
+  /*  $vendi = $_POST["vendi"];*/
+    $query = "INSERT INTO shfytezuesi(Emri,Mbiemri,idFoto,Email,Foto) values
+     ('$emri','$mbiemri','$idPhoto','$email','$foto')";
+     $query_run = mysqli_query($conn,$query);
+     if($query_run)
+     {echo "Te dhenat jane shtuar me sukses";}
+     else{
+         echo "Te dhenat nuk jane shtuar";
+     }
+
+
+
+ 
+}
+?>
