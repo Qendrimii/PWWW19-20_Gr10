d9a655f8d1b6db1f3774ed58fc16d9aeec512be2
Qendrimii <37595347+Qendrimii@users.noreply.github.com>
2020-05-27 23:16:59 +0200

    Ndryshim ne detaje

--- php/form.php
+++ php/form.php
@@ -1,27 +1,25 @@
 <?php
- $vlera = true;
  $servername = "localhost:3306";
  $username = "root";
  $password = "";
  $db = "mydb";
- $vlera = true;
  $conn = mysqli_connect($servername,$username,$password,$db);
  if(!$conn)
  {echo "Nuk mund te lidhet me databazen";}
- 
 
-if(isset($_POST['submit']))
-{ 
- 
-    $foto = $_POST("foto");
-    $idPhoto = $_POST["fotoID"];
+
+if(isset($_POST['save']))
+{
+
+    $foto = $_POST["foto"];
+    $fotoID = $_POST["fotoID"];
     $emri = $_POST["emri"];
     $mbiemri = $_POST["mbiemri"];
     $email = $_POST["email"];
   /*  $tema = $_POST["tema"];*/
   /*  $vendi = $_POST["vendi"];*/
     $query = "INSERT INTO shfytezuesi(Emri,Mbiemri,idFoto,Email,Foto) values
-     ('$emri','$mbiemri','$idPhoto','$email','$foto')";
+     ('$emri','$mbiemri','$fotoID','$email','../foto/$foto')";
      $query_run = mysqli_query($conn,$query);
      if($query_run)
      {echo "Te dhenat jane shtuar me sukses";}
